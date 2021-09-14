8e5f241c626957888c6a531bfddc9d429a07aead
yanickp <yanickpalmers@gmail.com>
2021-09-14 20:09:15 +0200

    mvc model

---
+++ ControllerTest/UnitTest1.fs
@@ -0,0 +1,11 @@
+module ControllerTest
+
+open NUnit.Framework
+
+[<SetUp>]
+let Setup () =
+    ()
+
+[<Test>]
+let Test1 () =
+    Assert.Pass()
---
+++ race simulator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace race_simulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
